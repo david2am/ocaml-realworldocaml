891b58531b5a093b09f99ecb453c9c1a0e8635c8
David <David.De.Vargas.Cueter.-ND@disney.com>
2025-06-07 16:32:30 -0500

    Refact: open mod_03 from main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,16 @@
+open Mod_03.Render_table
+
+let header = ["language";"architect";"first release"]
+let rows = [
+  ["Lisp" ;"John McCarthy" ;"1958"] ;
+  ["C"    ;"Dennis Ritchie";"1969"] ;
+  ["ML"   ;"Robin Milner"  ;"1973"] ;
+  ["OCaml";"Xavier Leroy"  ;"1996"] ;
+]
 
 let () =
   print_endline "\nHola mundo!"
+;;
+
+render_table ~header ~rows
+|> print_endline
